2957411b2739596c888cb2255dca994cad9f1620
youngkaya <iamjaehyun4@gmail.com>
2022-06-22 14:15:27 +0900

    Templates

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,10 +1,34 @@
-export const trending = (req, res) => res.send("Home Page Videos");
-export const see = (req, res) => {
-  return res.send(`Watch Video #${req.params.id}`);
-};
-export const edit = (req, res) => {
-  return res.send("Edit");
-};
+export const trending = (req, res) => {
+  let videos = [
+    {
+      title: "First Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1
+    },
+    {
+      title: "Second Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 2
+    },
+    {
+      title: "Third Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 3
+    }
+  ];
+  return res.render("home", { pageTitle: "Home", videos });
+}
+export const see = (req, res) => res.render("watch");
+export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => {
